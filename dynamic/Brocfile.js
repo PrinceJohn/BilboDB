519d77c6fab625239cc95db1732f8987dbadbe1a
Andreas Hansson <cyber_ante@hotmail.com>
2015-03-12 11:41:33 +0100

    bower components

--- dynamic/Brocfile.js
+++ dynamic/Brocfile.js
@@ -18,6 +18,7 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 // app.import('bower_components/isotope/isotope.pkgd.min.js');
 // app.import('bower_components/masonry/masonry.js');
-app.import('bower_components/tablesorter/jquery.tablesorter.js');
+app.import('public/lib/tablesorter/jquery.tablesorter.js');
+app.import('public/lib/isotope/isotope.js');
 
 module.exports = app.toTree();
